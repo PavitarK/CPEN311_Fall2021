1e782c64d14b929e7498ec0cf4f23058eca6b8a0
Ahnaf39 <39895710+Ahnaf39@users.noreply.github.com>
2021-03-09 15:15:55 -0800

    Add files via upload
    added task fsm

---
+++ lab4/Lab4_template_de1soc/template_de1soc/task1_fsm.sv
@@ -0,0 +1,28 @@
+module task1_fsm(s, done_flag);
+output reg [7:0] s[256];
+output done_flag;
+
+parameter start = 5'b00000;
+parameter array_fill = 5'b00010;
+parameter counter_inc = 5'b00100;
+parameter done = 5'b00001;
+
+reg [4:0] state = start;
+reg [7:0] counter = 0;
+assign done_flag = state[0];
+
+always @(*) begin
+    case(state)
+        start: begin state <= array_fill; end
+        array_fill: begin 
+                        state <= counter_inc;
+                        s[counter] <= counter;
+                    end
+        counter_inc: begin 
+                        if(counter <= 8'd255) state <= array_fill;
+                        else if (counter > 8'255) state <= done;
+                        counter <= counter + 1;
+                     end
+        done: begin state <= done; end
+end
+
