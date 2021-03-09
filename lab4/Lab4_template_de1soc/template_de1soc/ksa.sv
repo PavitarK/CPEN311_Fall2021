2bcca1c61364bfbadeeebedc77be7a7f15cbf873
Pavitar Kalra <me@pavitar.ca>
2021-03-09 15:11:26 -0800

    converted top file to sv

---
+++ lab4/Lab4_template_de1soc/template_de1soc/ksa.sv
@@ -0,0 +1,48 @@
+`default_nettype none
+
+module ksa(CLOCK_50, KEY, SW, LEDR, 
+           HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
+
+//=======================================================
+//  PORT declarations
+//=======================================================
+
+//////////// CLOCK //////////
+input                       CLOCK_50;
+
+//////////// LED //////////
+output           [9:0]      LEDR;
+
+//////////// KEY //////////
+input            [3:0]      KEY;
+
+//////////// SW //////////
+input            [9:0]      SW;
+
+//////////// SEG7 //////////
+output           [6:0]      HEX0;
+output           [6:0]      HEX1;
+output           [6:0]      HEX2;
+output           [6:0]      HEX3;
+output           [6:0]      HEX4;
+output           [6:0]      HEX5;
+
+//=======================================================
+//  REG/WIRE declarations
+//=======================================================
+// Input and output declarations
+logic CLK_50M;
+logic  [9:0] LED;
+assign CLK_50M =  CLOCK_50;
+assign LEDR[9:0] = LED[9:0];
+
+    logic clk; 
+    logic reset_n; 
+
+    assign reset_n = KEY[3]; 
+
+    //SevenSegmentDisplayDecoder sseg(.ssOut(), .nIn()); 
+
+
+
+endmodule 
