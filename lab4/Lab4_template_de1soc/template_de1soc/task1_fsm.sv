fda0173de077382a657550b7cfdcfc9673f24341
Pavitar Kalra <me@pavitar.ca>
2021-03-09 18:19:31 -0800

    ahnaf debug

--- lab4/Lab4_template_de1soc/template_de1soc/task1_fsm.sv
+++ lab4/Lab4_template_de1soc/template_de1soc/task1_fsm.sv
@@ -1,3 +1,5 @@
+`default_nettype none
+
 module task1_fsm(clk, s, done_flag);
 input clk; 
 output reg [7:0] s[256];
@@ -9,26 +11,29 @@ parameter counter_inc = 5'b00100;
 parameter done = 5'b00001;
 
 reg [4:0] state = start;
-reg [7:0] counter = 0;
+reg [7:0] counter = 8'd50;
 logic [7:0] out_mem; 
+logic wren = 0;
 assign done_flag = state[0];
- 
+assign wren = state[1];
 
-
-s_memory RAM1(.address(counter), .clock(clk), .data(counter), .wren(state[1]), .q(out_mem));
+s_memory RAM1(.address(counter), .clock(clk), .data(counter), .wren(wren), .q(out_mem));
 
 always @(posedge clk) begin
     case(state)
         start: begin state <= array_fill; end
         array_fill: begin 
-                        state <= counter_inc;
-                        s[counter] <= counter;
-                    end
-        counter_inc: begin 
-                        if(counter <= 8'd255) state <= array_fill;
-                        else if (counter > 8'd255) state <= done;
+                        //state <= counter_inc;
                         counter <= counter + 1;
-                     end
+                        s[counter] <= counter;
+                        if(counter <= 8'd255) state <= array_fill;
+                        else state <= done;
+                    end
+        // counter_inc: begin 
+        //                 if(counter <= 8'd255) state <= array_fill;
+        //                 else if (counter > 8'd255) state <= done;
+
+        //              end
         done: begin state <= done; end
         default: begin 
             state <= 5'bzzzzz; 
