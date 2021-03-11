60635508399c6479cc53ceff50da473471f22ca8
Ahnaf39 <ahnaf452000@gmail.com>
2021-03-10 16:55:26 -0800

    update task2a_fsm

--- lab4/Lab4_template_de1soc/template_de1soc/task2_fsm.sv
+++ lab4/Lab4_template_de1soc/template_de1soc/task2_fsm.sv
@@ -1,10 +1,12 @@
 `default_nettype none
 
-module task2_fsm(clk, s, fsm1_done, secret_key, done_flag);
+module task2_fsm(clk, s, fsm1_done, secret_key, swap_done, swap_flag, done_flag);
 input clk;
 input fsm1_done;
 input reg [7:0] s[256];
 input reg [7:0] secret_key[3];
+input swap_done
+output swap_flag;
 output done_flag;
 logic [7:0] counter_i = 0;
 logic [7:0] counter_j = 0;
@@ -17,6 +19,7 @@ parameter done = 5'b00001;
 
 reg [4:0] state = start;
 assign done_flag = state[0];
+assign swap_flag = state[3]
 
 always_ff @(posedge clk) begin
     case(state)
@@ -35,6 +38,20 @@ always_ff @(posedge clk) begin
             counter_i <= counter_i + 1;
             state <= swap_state;
         end
+        swap_state:
+        begin
+            // call some swap fsm
+            if(!swap_done) state <= swap_state;
+            else if(counter <= 8'd255 && swap_done) state <= j_logic;
+            else state <= done;
+        end
+        done: state <= done;
+        default: 
+        begin
+            state <= 5'bzzzzz;
+            counter_i <= counter_i;
+            counter_j <= counter_j;
+        end 
     endcase
 end
 endmodule
